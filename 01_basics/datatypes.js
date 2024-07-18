50383f3c8cbf5c08445607fc4b1abfb19c7dd487
SACHIN BIJALWAN <124330995+sachinbijalwan2892@users.noreply.github.com>
2024-07-18 04:53:35 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; //treat all js code as newer version
+
+//alert("hello") //we are using node js not browsers v8 engine
+
+
+console.log(3+3)
+//code readability should be high
+console.log("hello")
+
+
+//datatypes 
+let name = "Sachin" //string
+let age = 20 //number
+let isLoggedIn = false //boolean
+
+//primitive datatypes
+//number => 2 to power 53
+//big int => wide range of big nmbers
+//string => ""
+//boolean => true/false
+//null => standslone value 
+//undefined => value is not assigned yet 
+//symbols => unique
+
+
+//objects 
+
+
+console.log(typeof null)//null is an object 
+console.log(typeof undefined)//undefined has special type undefined 
+
+
