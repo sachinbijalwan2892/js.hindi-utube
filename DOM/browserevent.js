9bdeb1c76fbfc993d32e949d2f59eafc90af1a9b
SACHIN BIJALWAN <124330995+sachinbijalwan2892@users.noreply.github.com>
2024-07-25 05:03:24 +0000

    broswerevent

--- DOM/browserevent.js
+++ DOM/browserevent.js
@@ -45,3 +45,72 @@ of eventtarget and nodes
 //event-type->click
 //function->what to do when event happen
 
+
+
+//removeeventlistner
+
+//== loose equality // only checks value 
+//=== strict equality //checks value as well as dataype
+
+//==  -> allows type coersion means js will try to convert the items
+//to the compared type
+
+//=== prevent type coersion
+
+//if u want to apply the removeevent function u have to apply same function
+//to the removeeventlistner that u apply to addevent listner
+
+
+//note->functions are objects in javascript hence they will have different
+//memory addesses even if they are doing same thing or giving same value
+
+//conditions for removeeventlistner to run succesfully
+//same target
+//same datatype
+//same function
+
+
+//phases of an event
+/*
+1. capturing phase->founding the eventtarget
+2. at target phase->find the target successfully
+3. bubbling phase->going back after finding the target
+*/
+
+
+//by default your event listner will execute on bubbling phase
+
+//addEventListener(type,function,usecapture or not)
+
+
+//The event object
+/*
+when an event occurs, addeventlistner funtion gets an even object that 
+has lots of info inside it about that event
+
+if u want to check what is inside event 
+a.addeventlistner('click',function(s){
+       console.log(s);
+})
+
+
+*/
+
+//THE DEFAULT ACTION
+/*
+when ever u want to prevent any action like opening link display image 
+on clicking etc and u dont want these things to happen
+u can use preventdefault action it will prevent the objects default action
+
+*/
+
+
+//AVOID TOO MANY EVENTS
+
+/*
+if u are doing different functions givng same output or doing same thing 
+by an event then it would be better if all your events that want same thing 
+by a function would refer to only one function then it would be otpimised annd 
+would take lesser memory 
+avoid to many events
+*/
